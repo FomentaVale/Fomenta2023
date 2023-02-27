21c12a125b60d0c71643281a4834706624fb29e4
João Pedro Caetano <caetanojpo@gmail.com>
2023-02-27 13:04:26 -0300

    removi as rotas e pastas já preparadas

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/oqueSomos" element={<OqueSomos />} />
         <Route path="/quemSomos" element={<QuemSomos />} />
-        <Route path="/realizacoes" element={<Realizacoes />} />
-        <Route path="/startups" element={<Startups />} />
-        <Route path="/facaParte" element={<FacaParte />} />
         <Route path="/*" element={<div>Página não encontrada</div>} />
-        <Route path="/politica" element={<Politica />} />
       </Routes>
       <Rodape />
     </BrowserRouter>
