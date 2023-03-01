0f382e475b4c2db9117c324f4ae64d2472d9f009
João Pedro Caetano <caetanojpo@gmail.com>
2023-03-01 17:38:18 -0300

    rota da pagina o que somos

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/home';
 import QuemSomos from './pages/QuemSomos/quemSomos';
 import Rodape from './components/rodape/rodape';
 import Realizacoes from './pages/Realizacoes/realizacoes';
+import OqueSomos from './pages/OqueSomos/oQueSomos';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/oqueSomos" element={<OqueSomos />} />
         <Route path="/quemSomos" element={<QuemSomos />} />
         <Route path="/realizacoes" element={<Realizacoes />} />
         <Route path="/*" element={<div>Página não encontrada</div>} />
