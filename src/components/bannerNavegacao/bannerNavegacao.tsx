02c198c790bda32d443123ee25ea1dfc66da2f8f
Lucas <lucasbrancalhao454@gmail.com>
2023-03-02 11:28:50 -0300

    Componentização da página refatorada com array de objetos. Foi também adicionado a propriedade children para adicionar textos maiores dentro da tag banner. Será trabalhada a partir de agora o grid da do componente renderizado.

--- src/components/bannerNavegacao/bannerNavegacao.tsx
+++ src/components/bannerNavegacao/bannerNavegacao.tsx
@@ -1,65 +1,35 @@
-import { Flex, Heading, Text } from '@chakra-ui/react';
+import { Flex, Grid, GridItem, Heading, Text } from '@chakra-ui/react';
+import { PropsWithChildren } from 'react';
 import Botao from '../botao/botao';
 
-const caminhosBotoesPaginas = {
-  tituloBotao: [
-    ['Cursos', 'Hackaton', 'Geladatech', 'Podcast', 'Formação de Inovadores'],
-    ['Banco de Talentos', 'Apoiadores', 'Estagiários', 'Incubação']
-  ],
-  caminho: [
-    ['#cursos', '#hackaton', '#geladatech', '#podcast', '#formacaoInovadores'],
-    ['#bancoTalentos', '#apoiadores', '#estagiarios', '#incubacao']
-  ]
-};
+interface InformacoesBotoes {
+  tituloBotao: string;
+  conteudoReferencia: string;
+}
 
 interface BannerNavegacaoProps {
   tituloBanner: string;
-  subtituloBanner: string;
-  tipoPagina: string;
+  informacoesBotoesNavegacao: Array<InformacoesBotoes>;
 }
 
-const geraBotoesDinamicamente = (
-  props: BannerNavegacaoProps,
-  caminhosBotoesPaginas: any
-) => {
-  let botoes = [];
-
-  console.log(caminhosBotoesPaginas);
-
-  if (props.tipoPagina == 'realizacoes') {
-    botoes = caminhosBotoesPaginas.tituloBotao[0].map(
-      (titulo: string, index: number) => (
-        <Botao
-          key={index}
-          href={caminhosBotoesPaginas.caminho[0][index]}
-          descricao={titulo}
-          borda="solid 2px rgba(255, 255, 255, 0.5)"
-        />
-      )
-    );
-  } else if (props.tipoPagina == 'facaParte') {
-    botoes = caminhosBotoesPaginas.tituloBotao[1].map(
-      (titulo: string, index: number) => (
-        <Botao
-          key={index}
-          href={caminhosBotoesPaginas.caminho[1][index]}
-          descricao={titulo}
-          cor="cor.P3"
-          borda="solid 2px rgba(255, 255, 255, 0.5)"
-        />
-      )
-    );
-  }
-
-  return botoes;
-};
-
-export default function BannerNavegacao(props: BannerNavegacaoProps) {
+export default function BannerNavegacao(
+  props: PropsWithChildren<BannerNavegacaoProps>
+) {
   return (
-    <Flex alignItems="center" flexDir="column">
+    <Flex flexDirection={'column'}>
       <Heading>{props.tituloBanner}</Heading>
-      <Text>{props.subtituloBanner}</Text>
-      {geraBotoesDinamicamente(props, caminhosBotoesPaginas)}
+      <Grid templateColumns="repeat(5, 1fr)" gap={3}>
+        {props.informacoesBotoesNavegacao.map((item, index) => {
+          return (
+            <Botao
+              key={index}
+              href={item.conteudoReferencia}
+              descricao={item.tituloBotao}
+            />
+          );
+        })}
+      </Grid>
+      <Text>{props.children}</Text>
     </Flex>
   );
 }
--- src/pages/Realizacoes/realizacoes.tsx
+++ src/pages/Realizacoes/realizacoes.tsx
@@ -1,16 +1,45 @@
 import { Center, Heading, Text, Flex } from '@chakra-ui/react';
+import BannerNavegacao from '../../components/bannerNavegacao/bannerNavegacao';
 
 export default function Realizacoes() {
+  const informacoesBotoesNavegacao = [
+    {
+      tituloBotao: 'Cursos',
+      conteudoReferencia: '#cursos'
+    },
+    {
+      tituloBotao: 'Hackathon',
+      conteudoReferencia: '#hackathon'
+    },
+    {
+      tituloBotao: 'Geladatech',
+      conteudoReferencia: '#geladatech'
+    },
+    {
+      tituloBotao: 'Podcast',
+      conteudoReferencia: '#podcast'
+    },
+    {
+      tituloBotao: 'Formação de Inovadores',
+      conteudoReferencia: '#formacaoDeInovadores'
+    }
+  ];
+
   return (
     <Flex color="red.400" justifyContent="center" flexDir={{ base: 'column' }}>
       <Center>
-        <Heading>Realizações da Fomenta Vale</Heading>
+        <BannerNavegacao
+          tituloBanner="Realizações da Fomenta Vale"
+          informacoesBotoesNavegacao={informacoesBotoesNavegacao}
+        >
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+            facilis excepturi voluptatem nemo ducimus! Debitis ex odio nostrum
+            in, iure minima hic aperiam voluptas fuga nam quod quaerat voluptate
+            ipsum.
+          </Text>
+        </BannerNavegacao>
       </Center>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facilis
-        excepturi voluptatem nemo ducimus! Debitis ex odio nostrum in, iure
-        minima hic aperiam voluptas fuga nam quod quaerat voluptate ipsum.
-      </Text>
     </Flex>
   );
 }
